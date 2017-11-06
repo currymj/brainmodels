c1e7ce03255f0287d625a7c63a7b773401b44876
Michael J. Curry <currymj@umd.edu>
2017-11-06 17:02:55 -0500

    first commit

---
+++ R/getdata.R
@@ -0,0 +1,24 @@
+library(rio)
+library(tidyverse)
+library(Matrix)
+download_data <- function() {
+  dir.create("data", showWarnings = FALSE)
+  system("cd data; bash ../download.sh")
+}
+
+import_data <- function() {
+  matfiles <- list.files("data", pattern = "*.mat")
+  raw_adjacency_matrices <- map(matfiles, ~ import(file.path("data",.), "MAT")$fibergraph)
+  return(raw_adjacency_matrices)
+}
+process_single_matrix <- function(cmat) {
+  res <- cmat > 0
+  res <- res | t(res)
+  diag(res) <- 0
+  return(as.numeric(res))
+}
+
+process_data <- function(to_process) {
+
+  return(map(to_process, process_single_matrix))
+}
