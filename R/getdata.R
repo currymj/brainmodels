4cba2818986fba6aa2241b749a81977baff02651
Michael J. Curry <currymj@umd.edu>
2017-11-06 17:29:49 -0500

    namespace conflict

--- R/getdata.R
+++ R/getdata.R
@@ -8,14 +8,14 @@ download_data <- function() {
 
 import_data <- function() {
   matfiles <- list.files("data", pattern = "*.mat")
-  raw_adjacency_matrices <- map(matfiles, ~ import(file.path("data",.), "MAT")$fibergraph)
+  raw_adjacency_matrices <- map(matfiles, ~ rio::import(file.path("data",.), "MAT")$fibergraph)
   return(raw_adjacency_matrices)
 }
 process_single_matrix <- function(cmat) {
   res <- cmat > 0
   res <- res | t(res)
   diag(res) <- 0
-  return(as.numeric(res))
+  return(as(res, "dgCMatrix"))
 }
 
 process_data <- function(to_process) {
